375ba004b88db64c5a0964520c62eb87348daf2b
billstam12 <billstam12@gmail.com>
2021-01-21 16:05:19 +0200

    bean

--- lod-mapper/src/main/java/org/imsi/lod_mapper/Mapper.java
+++ lod-mapper/src/main/java/org/imsi/lod_mapper/Mapper.java
@@ -183,7 +183,7 @@ public class Mapper implements Serializable {
         	}
             return rdfs.iterator();
         }, Encoders.bean(RDF.class));
-        rdfDataset.show(false);
+        rdfDataset.take(10);
     }
 
 	private static SparkSession setupSparkSession() {
