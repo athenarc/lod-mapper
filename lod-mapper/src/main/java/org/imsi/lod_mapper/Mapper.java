069e41a1cd8744a7fbe7b5b60186fc26e57e0b3b
billstam12 <billstam12@gmail.com>
2021-01-21 14:12:23 +0200

    changes

--- lod-mapper/src/main/java/org/imsi/lod_mapper/Mapper.java
+++ lod-mapper/src/main/java/org/imsi/lod_mapper/Mapper.java
@@ -168,19 +168,20 @@ public class Mapper implements Serializable {
         //datasourceRecords.show();  
         Dataset<RDF> rdfDataset = groupedRecords.flatMap((FlatMapFunction<Row, RDF>) row -> {
         	System.out.println(row);
-        	List<String> columnsI = broadcastColumns.getValue().getColumns();
         	List<RDF> rdfs = new ArrayList<>();
-        	String rowId = row.getString(0);
-//        	System.out.println("id" + rowId);
-        	for (int i = 1; i < columnsI.size(); i++) {
-        		 List<String> col = row.getList(i);
-//        		 System.out.println("Col Val" + col);
-        		 for(int j = 0; j < col.size(); j++) {
-        			 RDF rdf = new RDF(rowId, columns.get(i), col.get(j));
-        			 System.out.println(rdf);
-        			 rdfs.add(rdf);
-        		 }
-        	}
+
+//        	List<String> columnsI = broadcastColumns.getValue().getColumns();
+//        	String rowId = row.getString(0);
+////        	System.out.println("id" + rowId);
+//        	for (int i = 1; i < columnsI.size(); i++) {
+//        		 List<String> col = row.getList(i);
+////        		 System.out.println("Col Val" + col);
+//        		 for(int j = 0; j < col.size(); j++) {
+//        			 RDF rdf = new RDF(rowId, columns.get(i), col.get(j));
+//        			 System.out.println(rdf);
+//        			 rdfs.add(rdf);
+//        		 }
+//        	}
             return rdfs.iterator();
         }, Encoders.bean(RDF.class));
         rdfDataset.show(false);
