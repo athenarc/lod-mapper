354bf65ad13be918e8adb6c3dff6cdcb0d1e2aa0
Giorgos Alexiou <galexiou@gmail.com>
2021-03-06 12:30:07 +0200

    authors subjects fixed for loop 1

--- lod-mapper/src/main/java/org/imsi/lod_mapper/model/SingleTTL.java
+++ lod-mapper/src/main/java/org/imsi/lod_mapper/model/SingleTTL.java
@@ -31,7 +31,7 @@ public class SingleTTL implements Serializable {
             int obSize = objects.size();
             int obCounter = 0;
             for (String object : objects) {
-                if (counter < size - 1 && obCounter < obSize - 1) {
+                if (counter < size - 1 && obCounter < obSize - 1 && obSize > 1) {
                     if (counter < size - 1) {
                         ttlString += "<" + key + "> " + object + "; ";
                     } else {
